938df01eeda5923e77f7a842e06d364d68e3a7b8
George Travis <wewerelikesharks@gmail.com>
2021-02-23 08:52:02 -0800

    initial commit, server sends string ok

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Cors = require('cors');
+const Cards = require('./dbCards');
+
+// App Config
+const app = express();
+const port = process.env.PORT || 8001;
+// const connection_url =
+//   'mongodb+srv://admin:1hTGxEfJWTvUQuvX@cluster0.id5cd.mongodb.net/tinderdb?retryWrites=true&w=majority';
+
+// Middleware
+app.use(express.json());
+app.use(Cors());
+
+// DB Config
+// mongoose.connect(connection_url, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// });
+
+// API endpoints
+app.get('/', (req, res) => res.status(200).send('catalog everything!'));
+
+// app.post('/tinder/cards', (req, res) => {
+//   const dbCard = req.body;
+//   Cards.create(dbCard, (err, data) => {
+//     if (err) {
+//       res.status(500).send(err);
+//     } else {
+//       res.status(201).send(data);
+//     }
+//   });
+// });
+
+// app.get('/tinder/cards', (req, res) => {
+//   Cards.find((err, data) => {
+//     if (err) {
+//       res.status(500).send(err);
+//     } else {
+//       res.status(200).send(data);
+//     }
+//   });
+// });
+
+// Listener
+app.listen(port, () => console.log(`listening on localhost: ${port}`));
