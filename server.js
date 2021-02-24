4b9fe94680add3a6f71ea67a6b8a1a4ba44851f7
George Travis <wewerelikesharks@gmail.com>
2021-02-23 17:06:52 -0800

    testing db connection

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ const app = express();
 const port = process.env.PORT || 8001;
 const connection_url =
   'mongodb+srv://admin:bFYrwUjiS@73-SF@cluster0.raeoc.mongodb.net/catalogdb?retryWrites=true&w=majority';
-//   'mongodb+srv://admin:bFYrwUjiS@73-SF@cluster0.id5cd.mongodb.net/catalogdb?retryWrites=true&w=majority';
-
-// mongodb+srv://admin:bFYrwUjiS@73-SF@cluster0.raeoc.mongodb.net/catalogdb?retryWrites=true&w=majority
 
 // Middleware
 app.use(express.json());
@@ -26,26 +23,26 @@ app.use(Cors());
 // API endpoints
 app.get('/', (req, res) => res.status(200).send('catalog everything!'));
 
-// app.post('/tinder/cards', (req, res) => {
-//   const dbCard = req.body;
-//   Cards.create(dbCard, (err, data) => {
-//     if (err) {
-//       res.status(500).send(err);
-//     } else {
-//       res.status(201).send(data);
-//     }
-//   });
-// });
+app.post('/tinder/cards', (req, res) => {
+  const dbCard = req.body;
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
 
-// app.get('/tinder/cards', (req, res) => {
-//   Cards.find((err, data) => {
-//     if (err) {
-//       res.status(500).send(err);
-//     } else {
-//       res.status(200).send(data);
-//     }
-//   });
-// });
+app.get('/tinder/cards', (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
 // Listener
 app.listen(port, () => console.log(`listening on localhost: ${port}`));
